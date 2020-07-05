2207bbb2c9f3303ab4ddc255fe9d9c59fb5d9944
BianXcn <wgliangjiawei@hotmail.com>
2020-07-05 23:20:30 +0800

    Version 1.2.2 Complete the LiteString class.

--- LiteWin32/LiteConfigManager.cpp
+++ LiteWin32/LiteConfigManager.cpp
@@ -1,7 +1,30 @@
 #include "LiteConfigManager.h"
 
-
 LiteConfigManager::LiteConfigManager()
+{
+
+
+
+
+
+
+
+}
+
+
+LiteConfigManager::LiteConfigManager(const char* location)
+{
+
+
+
+
+
+
+
+}
+
+
+LiteConfigManager::~LiteConfigManager()
 {
 
 
--- LiteWin32/LiteString.cpp
+++ LiteWin32/LiteString.cpp
@@ -1,37 +1,74 @@
 #include "LiteString.h"
+#include <math.h>
 
 LiteString::LiteString()
 {
 
-	head = new lite_word;
-	head->next = nullptr;
-	length  = 0;
+    head = new string_node;
+    head->next = NULL;
+    head->data = 0;
+    length = 0;
+
 
 }
+
 
 LiteString::LiteString(const char* data)
 {
 
-	head = new lite_word;
-	head->next = nullptr;
-	length = 0;
+    head = new string_node;
+    head->next = NULL;
+    head->data = 0;
+    length = 0;
 
-	lite_word* cur = head;
+    string_node* cur = head;
+    int i = 0;
 
+    while (data[i] != 0) {
 
-	int i = 0;
-
-	while (data[i]!=0)
-	{
-		cur->letter = data[i];
-		cur->next = new lite_word;
-
-		cur = cur->next;
-
-	}
+        cur->data = data[i];
 
 
+        cur->next = new string_node;
+        cur = cur->next;
 
+        i++;
+
+    }
+
+    cur->data = 0;
+    cur->next = NULL;
+    length = i;
+
+
+}
+
+LiteString::LiteString(LiteString& data)
+{
+
+    head = new string_node;
+    head->next = NULL;
+    head->data = 0;
+
+    length = data.length;
+
+    string_node* outdata = data.head;
+    string_node* cur = head;
+
+    for (int i = 0; i < data.length; i++) {
+
+        cur->data = outdata->data;
+
+        cur->next = new string_node;
+        cur = cur->next;
+        outdata = outdata->next;
+
+    }
+
+    cur->next = new string_node;
+    cur = cur->next;
+    cur->data = 0;
+    cur->next = NULL;
 
 
 }
@@ -39,17 +76,184 @@ LiteString::LiteString(const char* data)
 LiteString::~LiteString()
 {
 
-	lite_word* cur = head;
-	lite_word* temp = cur;
+    string_node* cur = head;
+    string_node* temp = cur;
 
-	while (cur!=nullptr) {
+    while (cur != NULL) {
 
-		temp = cur->next;
-		delete cur;
-		cur = temp;
+        temp = cur->next;
+
+        delete cur;
+
+        cur = temp;
+
+    }
 
 
-	}
+}
 
+bool LiteString::equal(LiteString& data)
+{
+
+    string_node* out_cur = data.head;
+    string_node* cur = head;
+
+    if (data.length != length) {
+        return false;
+    }
+
+    while (cur != NULL) {
+
+        if (out_cur->data != cur->data) {
+            return false;
+        }
+
+        out_cur = out_cur->next;
+        cur = cur->next;
+
+    }
+
+    return true;
+
+
+}
+
+void LiteString::parseInt(int& num)
+{
+
+    // CheckFunction
+
+    bool whether_convent = true;
+
+    string_node* cur = head;
+    int i = length;
+
+    while (i > 0) {
+
+        if (cur->data < 48 || cur->data>57) {
+            whether_convent = false;
+            break;
+        }
+        cur = cur->next;
+        i--;
+
+    }
+
+    if (whether_convent == true) {
+
+        cur = head;
+
+        int output = 0;
+        int lh = length - 1;
+        while (lh >= 0) {
+
+            int helper = pow(10, lh);
+            output += helper * (cur->data - 48);
+
+            cur = cur->next;
+            lh--;
+
+        }
+
+        num = output;
+    }
+
+
+}
+
+void LiteString::parseChar(char*& output)
+{
+
+    string_node* data = head;
+
+    int i = 0;
+
+    while (data != NULL) {
+
+        output[i] = data->data;
+
+        i++;
+        data = data->next;
+
+    }
+
+
+}
+
+void LiteString::add(LiteString& data)
+{
+
+    string_node* temp = data.head;
+
+    string_node* cur = head;
+
+    while (cur != NULL) {
+
+        if (cur->data == 0) {
+            break;
+        }
+
+        cur = cur->next;
+    }
+
+
+    while (temp != NULL) {
+        cur->next = new string_node;
+        cur = cur->next;
+        cur->data = temp->data;
+
+        temp = temp->next;
+
+    }
+
+    length += data.length;
+
+
+}
+
+bool LiteString::letterDecide(char data)
+{
+
+    string_node* temp = head;
+
+    while (temp != NULL) {
+        if (temp->data == data) {
+            return true;
+        }
+        temp = temp->next;
+    }
+
+    return false;
+
+
+}
+
+bool LiteString::letterDecide(char data, int place)
+{
+
+
+    if (place + 1 > length) {
+        return false;
+    }
+
+    string_node* temp = head;
+    int i = 0;
+
+    while (temp != NULL) {
+
+        if (i == place) {
+            break;
+        }
+
+        temp = temp->next;
+        i++;
+
+    }
+
+    if (data == temp->data) {
+        return true;
+    }
+
+    return false;
 
 }
